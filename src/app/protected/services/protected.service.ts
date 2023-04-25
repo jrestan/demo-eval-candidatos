7bbdd45335b0204fe478e811b5714c246bfb1d37
Javier <lrestan@onpe.gob.pe>
2023-04-24 19:20:45 -0500

    commit 24/04 3

--- src/app/protected/services/protected.service.ts
+++ src/app/protected/services/protected.service.ts
@@ -16,11 +16,12 @@ export class ProtectedService {
   buscarInfoFinancieraPorDNI(dni: string): Observable<ISenInfoFinResponse>{
     
     const url = `${this.baseUrl}/financial/detail`;
+    const autorization = 'Bearer '+sessionStorage.getItem('token')||'';
         
     //const headers = new HttpHeaders().set('Authorization', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY4MjI5MTEzNSwiZXhwIjoxNjgyMzc3NTM1fQ.kndSiPy_79O7RPJeRZBe--kQNUV6llwNHId6CU_adB0BLwtbE3xuWNK6rsx6ci_DCQBUJcVnhZnbnphyjQU1fg');
     //const params = new HttpParams().set('document_type', 'D').set('document_number',dni);
 
-    const headers = new HttpHeaders().set('Authorization', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY4MjI5MTEzNSwiZXhwIjoxNjgyMzc3NTM1fQ.kndSiPy_79O7RPJeRZBe--kQNUV6llwNHId6CU_adB0BLwtbE3xuWNK6rsx6ci_DCQBUJcVnhZnbnphyjQU1fg');
+    const headers = new HttpHeaders().set('Authorization', autorization);
 
     const myObject: any = { document_type: 'D', document_number: dni};
     const httpParams: HttpParamsOptions = { fromObject: myObject } as HttpParamsOptions;
